02644fe03595829cd51d5dcc3c468823429dfdf6
yaasiinaxmed <yaskassoy@gmail.com>
2023-07-29 10:29:45 +0300

    setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import About from './components/About'
+import Skills from './components/Skills'
+import Works from './components/Works'
+import Contact from './components/Contact'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Hero/>
+      <About/>
+      <Skills/>
+      <Works/>
+      <Contact/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
